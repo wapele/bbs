e29ad78ae90af1753e7b3a770906b8d0916b39cc
chenxutan <386117932@qq.com>
2018-03-02 21:06:33 +0800

    Initial commit.

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <topmenu></topmenu>
+    <router-view/>
+    <myfooter></myfooter>
+  </div>
+</template>
+
+<script>
+import topmenu from './components/topmenu'   
+import myfooter from './components/footer'   
+export default {
+  name: 'App',
+  components:{topmenu,myfooter}
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/index/main.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <indextop></indextop>
+        <div class="layui-container">
+            <div class="layui-row layui-col-space15">
+                    <indexleft></indexleft>
+                    <indexright></indexright>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import indextop from '../../components/index/top'   
+import indexleft from '../../components/index/left'  
+import indexright from '../../components/index/right'  
+export default {
+  name: 'IndexMain',
+  data () {},
+  components:{indextop,indexleft,indexright}
+}
+</script>
